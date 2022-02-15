4e1dbced1140ab0966f6761655a72cf09ebf23b1
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-15 02:10:39 +0100

    Initial commit

---
+++ src/test/java/com/example/spacetourismserver/SpaceTourismServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacetourismserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceTourismServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
