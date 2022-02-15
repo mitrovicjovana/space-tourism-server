39f914da4a98f66a08df2f43d7f7686db3149baa
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-02-15 03:12:26 +0100

    Add entities

---
+++ src/main/java/com/example/spacetourismserver/model/Crew.java
@@ -0,0 +1,29 @@
+package com.example.spacetourismserver.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "crew")
+public class Crew {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true, columnDefinition = "VARCHAR(63)")
+    private String name;
+
+    @Column(name = "role", nullable = false, columnDefinition = "VARCHAR(63)")
+    private String role;
+
+    @Column(name = "bio", nullable = false, columnDefinition = "VARCHAR(1023)")
+    private String bio;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+}
